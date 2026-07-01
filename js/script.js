3335ddb89fd0d60c99fc5eb287e41140b61c0816
Jan Eisenga <lukani@live.nl>
2026-07-01 17:07:29 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -74,29 +74,16 @@ function DrawMap()
         for(let x=0; x<MapData.width; x++)
         {
             let Tile = Layer[y * MapData.width + x] - 1;
-
+// console.log(x, y, Tile);
             if(Tile < 0)
                 continue;
 
             let SourceX = (Tile % 4) * TILE_SIZE;
             let SourceY = Math.floor(Tile / 4) * TILE_SIZE;
 
-            Ctx.drawImage(
-
-                TileImage,
-
-                SourceX,
-                SourceY,
-
-                TILE_SIZE,
-                TILE_SIZE,
-
-                x * TILE_SIZE,
-                y * TILE_SIZE,
-
-                TILE_SIZE,
-                TILE_SIZE
-            );
+            Ctx.fillStyle = "white";
+Ctx.font = "12px Arial";
+Ctx.fillText(Tile, x * 50 + 10, y * 50 + 25);
         }
     }
 }
