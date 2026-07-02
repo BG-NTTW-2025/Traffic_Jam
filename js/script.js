1ccd3f659f40e31f1c1a50261da10984d42e0c1b
Jan Eisenga <lukani@live.nl>
2026-07-02 13:40:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,105 @@
+"use strict";
+
+/*************************************************/
+/* SECTION 1 - GLOBALS                           */
+/*************************************************/
+
+let TILE_WIDTH;
+let TILE_HEIGHT;
+
+let Canvas;
+let Ctx;
+
+let MapData;
+let TileImage;
+
+
+/*************************************************/
+/* SECTION 2 - INITIALIZE                        */
+/*************************************************/
+
+window.onload = Init;
+
+async function Init()
+{
+    Canvas = document.getElementById("GameCanvas");
+    Ctx = Canvas.getContext("2d");
+
+    await LoadMap();
+}
+
+
+/*************************************************/
+/* SECTION 3 - LOAD MAP                          */
+/*************************************************/
+
+async function LoadMap()
+{
+    //-------------------------------------------------
+    // TMJ laden
+    //-------------------------------------------------
+
+    const Response = await fetch("Maps/USAcity.tsj");
+
+    MapData = await Response.json();
+	TILE_WIDTH  = MapData.tilewidth;
+    TILE_HEIGHT = MapData.tileheight;
+
+    //-------------------------------------------------
+    // Canvas grootte aanpassen
+    //-------------------------------------------------
+
+    Canvas.width  = MapData.width  * TILE_WIDTH;
+    Canvas.height = MapData.height * TILE_HEIGHT;
+
+    //-------------------------------------------------
+    // PNG laden
+    //-------------------------------------------------
+
+    TileImage = new Image();
+
+    TileImage.src = "Art/USA_Roads.png";
+
+    TileImage.onload = DrawMap;
+}
+
+
+/*************************************************/
+/* SECTION 4 - DRAW MAP                          */
+/*************************************************/
+
+function DrawMap()
+{
+    let Layer = MapData.layers[0].data;
+
+    for(let y=0; y<MapData.height; y++)
+    {
+        for(let x=0; x<MapData.width; x++)
+        {
+            let Tile = Layer[y * MapData.width + x] - 1;
+//console.log(x, y, Tile);
+            if(Tile < 0)
+                continue;
+
+            let SourceX = (Tile % 4) * TILE_WIDTH;
+            let SourceY = Math.floor(Tile / 4) * TILE_HEIGHT;
+
+            Ctx.drawImage(
+
+                TileImage,
+
+                SourceX,
+                SourceY,
+
+                TILE_WIDTH,
+                TILE_HEIGHT,
+
+                x * TILE_WIDTH,
+                y * TILE_HEIGHT,
+
+                TILE_WIDTH,
+                TILE_HEIGHT
+            );
+        }
+    }
+}
