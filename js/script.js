87cb1aadca8ce090d0d05f39f208590808d6095f
Jan Eisenga <lukani@live.nl>
2026-07-02 13:21:40 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ async function LoadMap()
     // TMJ laden
     //-------------------------------------------------
 
-    const Response = await fetch("Maps/USAcity.tmj");
+    const Response = await fetch("Maps/USAcity.tsj");
 
     MapData = await Response.json();
 
