326b5c5237d238439f1304d4cc6e716760cbaa59
Jan Eisenga <lukani@live.nl>
2026-07-02 06:20:30 +0200

    Add files via upload

---
+++ Maps/USA_Roads.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="USA_Roads" tilewidth="50" tileheight="50" tilecount="16" columns="4">
+ <image source="../Art/USA_Roads.png" width="200" height="200"/>
+ <tile id="0">
+  <properties>
+   <property name="Exit" value="S,W"/>
+  </properties>
+ </tile>
+</tileset>
