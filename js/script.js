5b049daa3cc312517a5f595f3b95948939dc2e35
Jan Eisenga <lukani@live.nl>
2026-07-08 11:19:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 let TilesetData;
 
 let TileInfo = [];
-const VERSION = "v0.1.86";
+const VERSION = "v0.1.87";
 let TILE_WIDTH;
 let TILE_HEIGHT;
 
@@ -173,7 +173,7 @@ const EAST  = 1;
 const SOUTH = 2;
 const WEST  = 3;
 
-const QUEUE_DISTANCE_BEFORE_CENTER = -10;
+const QUEUE_DISTANCE_BEFORE_CENTER = 0;
 
 const MAX_CARS = 40;
 
